80d0447d41967ddbf9655d21153d50ced51c6bc0
shallwexuewei <wxuewei1991@gmail.com>
2015-10-20 21:22:34 -0400

    Update 32 Longest Valid Parentheses_DP.java

--- 32 Longest Valid Parentheses_DP.java
+++ 32 Longest Valid Parentheses_DP.java
@@ -1,16 +1,50 @@
 public class Solution {
     public int longestValidParentheses(String s) {
-        if (s == null || "".equals(s)) return 0;
-        int[] dp = new int[s.length() + 1];
-        int max = 0;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == ')' && i - dp[i] >= 1 && s.charAt(i - dp[i] - 1) == '(') {
-                dp[i + 1] = dp[i - dp[i] - 1] + dp[i] + 2;
+        int len = s.length();
+        
+        int[] dp = new int[len+1];
+        
+        if(len < 2) {
+            return 0;
+        }
+        int maxLen = 0;
+        
+        dp[0] = 0;
+        if(s.charAt(1) == ')' && s.charAt(0) == '('){
+            dp[1] = 2;
+            maxLen = 2;
+        }
+        
+        for(int i = 2; i < len; i++){
+            if(s.charAt(i) == '(') {
+                dp[i] = 0;
             }
+            else {
+                // ')'
+                if(s.charAt(i-1) == '(') {
+                    //System.out.println(s);
+                    dp[i] = dp[i-2] + 2;
+                }
+                else{
+                    // "))"
+                    int left = i-dp[i-1]-1;
+                    if(left > -1 && s.charAt(left) == '(') {
+                        dp[i] = i-left+1;
+                        if(left-1 > -1){
+                            dp[i] += dp[left-1];
+                        }
+                    }
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+                
+            }
+            /*
+            for(int j = 0; j < len; j++) {
+                System.out.print(dp[j]+",");
+            }
+            System.out.println();
+            */
         }
-        for (int i = 0; i < dp.length; i++) {
-            if (dp[i] > max) max = dp[i];
-        }
-        return max;
+        return maxLen;
     }
 }
