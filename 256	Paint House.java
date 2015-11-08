77089196d6395550eea55382c5026b039ec19fd9
shallwexuewei <wxuewei1991@gmail.com>
2015-11-07 23:19:45 -0500

    Update 256	Paint House.java
    add memo

--- 256	Paint House.java
+++ 256	Paint House.java
@@ -1,39 +1,48 @@
-public class Solution {
-    private int globalMin;
+public class Solution { 
     
     public int minCost(int[][] costs) {
         if(costs == null) {
             return 0;
         }
         
-        // depth first search
-        globalMin = Integer.MAX_VALUE;
+        // depth first search 
         int last = costs.length - 1;
         
         if(last < 0) {
             return 0;
-        }
+        } 
          
-        
-        return minCost(costs, 0, last, 0);
+        int[][] memo = new int[costs.length][costs[0].length];
+         
+        return minCost(costs, -1, 0, last, memo);
     }
     
-    public int minCost(int[][] costs, int current, int last, int soFarCost) {
-        // base case
-        if(current == last) {
-            int min = costs[last][0];
-            for(int i = 1; i < costs[last].length; i++) {
-                min = Math.min(min, costs[last][i]);
-            }
-            soFarCost += min;
-            globalMin = Math.min(globalMin, soFarCost);
-            return soFarCost;
+    public int minCost(int[][] costs, int prevColor, int current, int last, int[][] memo) {
+        if(prevColor != -1 && memo[current][prevColor] != 0){
+            return memo[current][prevColor];
         }
         
-        int min = minCost(costs, current + 1, last, soFarCost + costs[current][0]);
-        for(int i = 1; i < costs[current].length; i++) {
-            min = Math.min(min, minCost(costs, current + 1, last, soFarCost + costs[current][i]));
-        }
+        // base case
+        if(current == last) {
+            int min = Integer.MAX_VALUE;
+            for(int i = 0; i < costs[last].length; i++) {
+                if(i == prevColor) {
+                    continue;
+                }
+                min = Math.min(min, costs[last][i]);
+            }
+            return min;
+        } 
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < costs[current].length; i++) {
+            if(i == prevColor) {
+                continue;
+            }
+            min = Math.min(min, costs[current][i] + minCost(costs, i, current + 1, last, memo));
+        } 
+        if(prevColor != -1)
+            memo[current][prevColor] = min;
         return min;
     }
 }
