aad738a154dd2461220112e74255538b1723f7aa
shallwexuewei <wxuewei1991@gmail.com>
2015-11-11 10:12:07 -0500

    Update 264 Ugly Number II.java
    accepted

--- 264 Ugly Number II.java
+++ 264 Ugly Number II.java
@@ -1,8 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Solution {
-	public int nthUglyNumber(int n) {
+    public int nthUglyNumber(int n) {
+        if(n < 1) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        
         int i2 = 0;
         int i3 = 0;
         int i5 = 0;
@@ -12,45 +16,21 @@ public class Solution {
         uglyList2.add(2);
         uglyList3.add(3);
         uglyList5.add(5);
-        int cnt = 1;
+        int cnt = 2;
         int min = 0;
         while(cnt < n) {
             int c2 = uglyList2.get(i2);
             int c3 = uglyList3.get(i3);
             int c5 = uglyList5.get(i5);
-            min = c2;
-            if(c2 < c3) {
-                if(c2 < c5) {
-                    i2++; 
-                } else{
-                    i5++;
-                    min = c5;
-                }
-            } else {
-                // c2 > c3
-                if(c3 < c5) {
-                    i3++;
-                    min = c3;
-                } else{
-                    i5++;
-                    min = c5;
-                }
-            }
+            min = Math.min(Math.min(c2, c3), c5);
+            if(min == c2) i2++;
+            if(min == c3) i3++;
+            if(min == c5) i5++;
             uglyList2.add(min*2);
             uglyList3.add(min*3);
             uglyList5.add(min*5); 
             cnt++;
-            System.out.println(min);
-            System.out.println(Arrays.toString(uglyList2.toArray()));
-            System.out.println(Arrays.toString(uglyList3.toArray()));
-            System.out.println(Arrays.toString(uglyList5.toArray()));
-            System.out.println();
         }
         return Math.min(Math.min(uglyList2.get(i2), uglyList3.get(i3)), uglyList5.get(i5));
     }
-
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		System.out.println(s.nthUglyNumber(10));
-	}
 }
