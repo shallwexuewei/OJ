6e877a219501715ed0ba6509c90862cb09187559
shallwexuewei <wxuewei1991@gmail.com>
2015-11-18 15:02:40 -0500

    Update 270	Closest Binary Search Tree Value.java

--- 270	Closest Binary Search Tree Value.java
+++ 270	Closest Binary Search Tree Value.java
@@ -9,18 +9,22 @@
  */
 public class Solution {
     public int closestValue(TreeNode root, double target) {
-        return closestValue(root, target, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return closestValue(root, target, Integer.MAX_VALUE, Integer.MIN_VALUE, false, false);
     }
     
-    private int closestValue(TreeNode node, double target, int larger, int smaller) {
-        if(node == null) {
+    private int closestValue(TreeNode node, double target, int larger, int smaller, boolean largeC, boolean smallC) {
+        if(node == null) { 
+            if(!smallC)
+                return larger;
+            if(!largeC)
+                return smaller;
             return larger-target < target-smaller ? larger:smaller;
         }
         
         if(target > node.val) {
-            return closestValue(node.right, target, larger, node.val);
+            return closestValue(node.right, target, larger, node.val, largeC, true);
         } else {
-            return closestValue(node.left, target, node.val, smaller);
+            return closestValue(node.left, target, node.val, smaller, true, smallC);
         }
     }
 }
