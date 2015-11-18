e085279d3829bcce936385a8ba9f6b46c86d4078
shallwexuewei <wxuewei1991@gmail.com>
2015-11-18 01:59:48 -0500

    Update 293 Flip Game.java

--- 293 Flip Game.java
+++ 293 Flip Game.java
@@ -3,29 +3,17 @@ public class Solution {
         int len = s.length();
         List<String> result = new LinkedList<String>();
         
-        if(len <= 2) {
+        if(len < 2) {
             return result;
         }
         
-        char prev = s.charAt(0);
+        boolean prevPlus = s.charAt(0) == '+' ? true:false;
         for(int i = 1; i < len; i++) {
-            char current = s.charAt(i);
-            if(current == prev) {
-                char newC = '+';
-                if(current == '+') newC = '-';
-                if(i != 1 && i + 1 != len) {
-                    result.add(s.substring(0, i-1) + newC + newC + s.substring(i+1, len));
-                }
-                else if(i + 1 != len) {
-                    result.add(""+ newC + newC + s.substring(i+1, len));
-                }
-                else  if(i != 1 ) {
-                    result.add(s.substring(0, i-1) + newC + newC);
-                } else {
-                    result.add("" + newC + newC);
-                }
-            }
-            prev = current;
+            boolean current = s.charAt(i) == '+' ? true:false;
+            if(prevPlus && current) {
+                result.add(s.substring(0, i-1) + "--" + s.substring(i+1, len));
+            } 
+            prevPlus = current;
         }
         return result;
     }
