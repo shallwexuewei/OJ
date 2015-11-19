19102627ad5546d7cac53075d664afec2a4831ba
shallwexuewei <wxuewei1991@gmail.com>
2015-11-19 16:05:45 -0500

    Update 261 Graph Valid Tree.java

--- 261 Graph Valid Tree.java
+++ 261 Graph Valid Tree.java
@@ -4,31 +4,36 @@ public class Solution {
     2. no cycle
     */
     public boolean validTree(int n, int[][] edges) {
-        ArrayList<LinkedList<Integer>> neighbors = new ArrayList<LinkedList<Integer>>(n);
+        ArrayList<HashSet<Integer>> neighbors = new ArrayList<HashSet<Integer>>(n);
         for(int i = 0; i < n; i++) {
-            neighbors.add(new LinkedList<Integer>());
+            neighbors.add(new HashSet<Integer>());
         }
         
         for(int[] edge:edges) {
-            if(edge[0] < edge[1]) {
-                neighbors.get(edge[0]).add(edge[1]);
-            } else {
-                neighbors.get(edge[1]).add(edge[0]);
-            }
+            neighbors.get(edge[0]).add(edge[1]);
+            neighbors.get(edge[1]).add(edge[0]);
         }
         
         
         HashSet<Integer> visited = new HashSet<Integer>();
         LinkedList<Integer> q = new LinkedList<Integer>();
+        LinkedList<Integer> prevs = new LinkedList<Integer>();
         
         q.add(0);
+        prevs.add(-1);
         while(!q.isEmpty()) {
             int current = q.removeFirst();
+            int prev = prevs.removeFirst();
             
             if(visited.contains(current)) return false;
             
             visited.add(current);
-            q.addAll(neighbors.get(current));
+            for(int i:neighbors.get(current)){
+                if(i != prev) {
+                    q.add(i);
+                    prevs.add(current);
+                }
+            }
         }
         
         if(visited.size() == n) {
