77005c53386276881d130429d2bede83e16afa56
shallwexuewei <wxuewei1991@gmail.com>
2015-11-20 00:34:57 -0500

    Create 226	Invert Binary Tree.java

---
+++ 226	Invert Binary Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        return root;
+    }
+}
+
+/*
+Wrong Answer More Details 
+
+Input:
+[4,2,7,1,3,6,9]
+Output:
+[4,7,2,6,9,1,3]
+Expected:
+[4,7,2,9,6,3,1]
+*/
