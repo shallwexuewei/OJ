012b8c4895315af98f0c9c35e588edc25f3b6c7b
shallwexuewei <wxuewei1991@gmail.com>
2015-11-23 20:18:28 -0500

    Update 277	Find the Celebrity.java

--- 277	Find the Celebrity.java
+++ 277	Find the Celebrity.java
@@ -7,25 +7,27 @@ public class Solution extends Relation {
             return -1;
         }
          
-        int[] inDegree = new int[n];
-        int[] outDegree = new int[n];
+        int left = 0;
+        int right = n - 1;
          
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && knows(i, j)) {
-                    outDegree[i]++;
-                    inDegree[j]++;
-                }
+        // Step 1: Find the potential candidate
+        while (left < right) {
+            if (knows(left, right)) {
+                left++;
+            } else {
+                right--;
             }
         }
          
+        // Step 2: Validate the candidate
+        int candidate = right;
         for (int i = 0; i < n; i++) {
-            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
-                return i;
+            if (i != candidate && (!knows(i, candidate) || knows(candidate, i))) {
+                return -1;
             }
         }
          
-        return -1;
+        return candidate;
     }
 }
 
