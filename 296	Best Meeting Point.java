e7b55bdd7b524d6bf27d6a2b5f141498371e2dc4
shallwexuewei <wxuewei1991@gmail.com>
2015-11-25 17:28:10 -0500

    Create 296	Best Meeting Point.java

---
+++ 296	Best Meeting Point.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int minTotalDistance(int[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        ArrayList<Integer> rowList = new ArrayList<Integer>();
+        ArrayList<Integer> colList = new ArrayList<Integer>();
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == 1) {
+                    rowList.add(r);
+                    colList.add(c);
+                }
+            }
+        }
+        
+        int sum = 0;
+        int midSize = rowList.size()/2;
+        int mr = rowList.get(midSize);
+        int mc = colList.get(midSize);
+        
+        for(int i = 0; i < rowList.size(); i++) {
+            sum += rowList.get(i) - mr;
+            sum += colList.get(i) - mc;
+        }
+        
+        return sum;
+    }
+}
