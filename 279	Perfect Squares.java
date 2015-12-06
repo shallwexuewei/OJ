00f1e0de817c6a0038d18bb286bb341efcd92ebd
shallwexuewei <wxuewei1991@gmail.com>
2015-12-06 01:50:00 -0500

    Update 279	Perfect Squares.java

--- 279	Perfect Squares.java
+++ 279	Perfect Squares.java
@@ -1,24 +1,30 @@
 public class Solution {
+    
     public int numSquares(int n) {
-        
-        double sqrt = Math.sqrt(n);
+        int[] memos = new int[n];
+        return numSquares(n, memos);
+    }
+    
+    private int numSquares(int n, int[] memos) { 
+        int sqrt = (int) Math.sqrt(n);
         if(sqrt * sqrt == n) {
             return 1;
         } else {
             int min = n;
-            for(int i = (int) sqrt; i > 0; i--) {
-                int temp = numSquares(n-i*i);
-                if(temp != 0) {
-                    if(min > temp + 1) {
-                        min = temp + 1;
-                    }
+            for(int i =  sqrt; i > 0; i--) {
+                int val = n - i*i;
+                int temp = memos[val];
+                if(temp == 0) {
+                    temp = numSquares(val, memos);
+                    memos[val] = temp;
+                }
+                if(min > temp + 1) {
+                    min = temp + 1;
                 }
             }
             return min;
         }
     }
-    
-    
 }
 
 /*
@@ -26,4 +32,14 @@ Submission Result: Time Limit Exceeded More Details
 
 Last executed input:
 229
+
+
+Submission Result: Wrong Answer More Details 
+
+Input:
+11
+Output:
+1
+Expected:
+3
 */
