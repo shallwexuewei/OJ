2997db7a7d80870a2e49a1cb8dcbd6b51583f07c
shallwexuewei <wxuewei1991@gmail.com>
2015-12-26 18:58:23 -0500

    Update 87. Scramble String.java

--- 87. Scramble String.java
+++ 87. Scramble String.java
@@ -4,7 +4,7 @@ public class Solution {
         if(s1 == null || s2 == null) {
             return false;
         }  
-        
+        System.out.println(s1 + ", " + s2);
         int len = s1.length();
         if(len != s2.length())  return false;
         
@@ -26,13 +26,18 @@ public class Solution {
             isScramble(s1.substring(i, len), s2.substring(i, len))) {
                 return true;
             }
-            if(isScramble(s1.substring(0, i), s2.substring(i, len)) && 
-            isScramble(s1.substring(i, len), s2.substring(0, i))) {
+            if(isScramble(s1.substring(0, i), s2.substring(len-i, len)) && 
+            isScramble(s1.substring(i, len), s2.substring(0, len-i))) {
                 return true;
             }
         }
         
         return false;
+    }
+    
+    public static void main(String[] args) {
+    	Solution s= new Solution();
+    	System.out.println(s.isScramble("abc", "bca"));
     }
 }
 
@@ -42,6 +47,26 @@ Submission Result: Wrong Answer More Details
 Input:
 "ab"
 "ba"
+Output:
+false
+Expected:
+true
+
+Submission Result: Wrong Answer More Details 
+
+Input:
+"abb"
+"bba"
+Output:
+false
+Expected:
+true
+
+Submission Result: Wrong Answer More Details 
+
+Input:
+"abc"
+"bca"
 Output:
 false
 Expected:
