33405a798e010a1af5754e163bc32327012a338f
shallwexuewei <wxuewei1991@gmail.com>
2016-01-10 16:29:31 -0500

    Update 239	Sliding Window Maximum.java

--- 239	Sliding Window Maximum.java
+++ 239	Sliding Window Maximum.java
@@ -1,42 +1,38 @@
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || k == 0) { 
+            return (new int[k]);
+        }
         int len = nums.length;
-        if(len < 1 || k < 1) {
-            return new int[0];
+        
+        int[] res = new int[len - k + 1];
+        
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        
+        if(len < k) {
+            /* */
         }
-        if(k == 1) {
-            return nums;
+        
+        for(int i = 0; i < k; i++) {
+            addIn(list, nums[i]);
         }
+        res[0] = list.get(0);
         
-        int winLen = len - k + 1;
-        int[] winMax = new int[winLen];
-        
-        LinkedList<Integer> maxList = new LinkedList<Integer>();
-        
-        maxList.add(nums[0]);
-        
-        for(int i = 1; i < len; i++) {
-            if(nums[i] > maxList.get(0)) {
-                maxList.clear();
-                maxList.add(nums[i]);
-            } else{
-                while(maxList.get(maxList.size()-1) < nums[i]){
-                    maxList.removeLast();
-                }
-                maxList.add(nums[i]);
+        for(int i = k; i < len; i++) {
+            if(nums[i-k] == list.get(0)){
+                list.removeFirst();
             }
-            if(i >= k) {
-                int deleted = i - k;
-                if(maxList.size() > 1 && maxList.get(0) == nums[deleted]){
-                    maxList.remove();
-                }
-                winMax[i-k+1] = maxList.get(0);
-            } else if(i == k-1) {
-                winMax[i-k+1] = maxList.get(0);
-            }
+            addIn(list, nums[i]);
+            res[i-k+1] = list.get(0);
         }
+        return res;
         
-        return winMax;
-        
+    }
+    
+    private void addIn(LinkedList<Integer> list, int val) {
+        while(!list.isEmpty() && list.get(list.size()-1) < val) { 
+            list.removeLast();
+        }
+        list.add(val);
     }
 }
