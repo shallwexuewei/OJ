a61681adefbb31738e54e7eb518e63abb7e5ba48
shallwexuewei <wxuewei1991@gmail.com>
2016-01-13 19:05:28 -0500

    Create 210. Course Schedule II.java

---
+++ 210. Course Schedule II.java
@@ -0,0 +1,86 @@
+public class Solution {
+    public class Course{
+        public int index;
+        public HashSet<Course> prevList;
+        public HashSet<Course> deeperList;
+        public Course(int i) {
+            index = i;
+            prevList = new HashSet<Course>();
+            deeperList = new HashSet<Course>();
+        }
+        
+        public void addPrev(Course prev) {
+            prevList.add(prev);
+            prev.addDeeper(this);
+        }
+        
+        public void addDeeper(Course deeper) {
+            deeperList.add(deeper);
+        }
+        
+        public boolean isPrevEmpty() {
+            return prevList.isEmpty();
+        }
+        
+        public void removePrev(Course prev) {
+            prevList.remove(prev);
+        }
+        
+        public ArrayList<Course> finish() {
+            ArrayList<Course> newFreeCourses = new ArrayList<Course>();
+            for(Course c:deeperList) {
+                c.removePrev(this);
+                if(c.isPrevEmpty()){
+                    newFreeCourses.add(c);
+                }
+            }
+            return newFreeCourses;
+        }
+        
+        public int getIndex() {
+            return this.index;
+        }
+    }
+    
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if(numCourses > 0)  {
+            // throw
+        }
+        
+        ArrayList<Course> courses = new ArrayList<Course>(numCourses); 
+        for(int i = 0; i < numCourses; i++) {
+            courses.add(new Course(i));
+        }
+        
+        for(int[] pre:prerequisites) {
+            courses.get(pre[0]).addPrev(courses.get(pre[1]));
+        }
+        
+        LinkedList<Course> freeCourses = new LinkedList<Course>();
+        for(Course course:courses) {
+            if(course.isPrevEmpty()){
+                freeCourses.add(course);
+            }
+        }
+        
+        int[] res = new int[numCourses];
+        int count = 0; // count the courses that have been finished
+        
+        // BFS
+        while(!freeCourses.isEmpty()) {
+            Course course = freeCourses.poll();
+            ArrayList newFreeCourses = course.finish();
+            res[count] = course.getIndex();
+            count++;
+            freeCourses.addAll(newFreeCourses);
+        }
+        
+        if(count == numCourses) {
+            return res;
+        } else {
+            return new int[0];
+        }
+        
+        
+    }
+}
