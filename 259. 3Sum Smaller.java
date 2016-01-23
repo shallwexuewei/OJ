e58b58f42b58c5a446bc160b4de60496dc6d8ffb
shallwexuewei <wxuewei1991@gmail.com>
2016-01-23 14:42:17 -0500

    Update 259. 3Sum Smaller.java

--- 259. 3Sum Smaller.java
+++ 259. 3Sum Smaller.java
@@ -2,14 +2,17 @@ public class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         int len = nums.length;
         int count = 0;
+        Arrays.sort(nums);
         for(int i = 0; i < len; i++) {
-            for(int j = i + 1; j < len; j++) {
-                int sum2 = nums[i] + nums[j];
-                for(int k = j + 1; k < len; k++) {
-                    int sum3 = sum2 + nums[k];
-                    if(sum3 < target) {
-                        count++;
-                    }
+            int sumTarget = target - nums[i];
+            int left = i + 1;
+            int right = len - 1;
+            while(left < right) {
+                if(nums[left] + nums[right] >= sumTarget) {
+                    right --;
+                } else {
+                    count += right - left;
+                    left++;
                 }
             }
         }
