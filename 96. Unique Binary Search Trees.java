7f08cd0e68dba8344baa716fce9f9095fe1718da
shallwexuewei <wxuewei1991@gmail.com>
2016-01-24 10:51:52 -0500

    Update 96. Unique Binary Search Trees.java

--- 96. Unique Binary Search Trees.java
+++ 96. Unique Binary Search Trees.java
@@ -1,15 +1,34 @@
 public class Solution {
     public int numTrees(int n) {
-        
-    int [] G = new int[n+1];
-    G[0] = G[1] = 1;
-
-    for(int i=2; i<=n; ++i) {
-        for(int j=1; j<=i; ++j) {
-            G[i] += G[j-1] * G[i-j];
-        }
+        int[] memos = new int[n+1];
+        return numTrees(n, memos);
     }
-
-    return G[n];
+    
+    private int numTrees(int n, int[] memos) {
+        if(n == 1) {
+            return 1;
+        } 
+        if(memos[n] > 0) {
+            return memos[n];
+        }
+        
+        
+        int sum = 0;
+        int half = n/2;
+        
+        sum += numTrees(n-1, memos);
+        int left = 1;
+        int right = n - 2;
+        for( ; left < half; left++) {
+            sum += numTrees(left, memos)*numTrees(right, memos);
+            right--;
+        }
+        sum *= 2;
+        if(n%2 == 1) {
+            int temp = numTrees(half, memos);
+            sum += temp*temp;
+        }
+        memos[n] = sum;
+        return sum;
     }
 }
