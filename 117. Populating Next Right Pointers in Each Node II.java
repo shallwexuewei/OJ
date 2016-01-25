23408c807a799281a02ed30d6f78dd0ea030e8d6
shallwexuewei <wxuewei1991@gmail.com>
2016-01-24 22:00:40 -0500

    Update 117. Populating Next Right Pointers in Each Node II.java

--- 117. Populating Next Right Pointers in Each Node II.java
+++ 117. Populating Next Right Pointers in Each Node II.java
@@ -10,28 +10,21 @@ public class Solution {
     public void connect(TreeLinkNode root) {
         if(root == null)    return;
         
-        LinkedList<TreeLinkNode> queue = new LinkedList<TreeLinkNode>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            TreeLinkNode prev = null;
-            for(int i = 0; i < size; i++) {
-                TreeLinkNode node = queue.poll();
-                if(node.left != null) {
-                    if(prev != null) {
-                        prev.next = node.left; 
-                    }
-                    prev = node.left;
-                    queue.add(node.left);
-                }
-                if(node.right != null) {
-                    if(prev != null) {
-                        prev.next = node.right; 
-                    }
-                    prev = node.right;
-                    queue.add(node.right);
-                }
+        TreeLinkNode fakeHead = new TreeLinkNode(0);
+        TreeLinkNode prev = fakeHead;
+        while(root != null) {
+            if(root.left != null) {
+                prev.next = root.left;
+                prev = root.left;
             }
+            if(root.right != null) {
+                prev.next = root.right;
+                prev = root.right;
+            }
+            root = root.next;
         }
+        
+        // fakeHead.next is the real head of the next level
+        connect(fakeHead.next);
     }
 }
