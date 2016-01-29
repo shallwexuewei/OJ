8c3bb42d4b3056fa1aed4e06f74f4b6917eced6e
shallwexuewei <wxuewei1991@gmail.com>
2016-01-28 22:34:08 -0500

    Update 4. Median of Two Sorted Arrays.java

--- 4. Median of Two Sorted Arrays.java
+++ 4. Median of Two Sorted Arrays.java
@@ -7,20 +7,23 @@ public class Solution {
             if(checkArray(nums2) == false) {
                 return 0.0;
             } else {
-                nums = nums2;
+                return findMedian(nums2);
             }
         }
         if(checkArray(nums2) == false) {
-            nums = nums1;
-        } else {
-            nums = merge(nums1, nums2);
-        }
-        int len = nums.length;
+            return findMedian(nums1);
+        } 
+        
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int len = len1 + len2;
         int half = len/2;
+        int last1 = len1 - 1;
+        int last2 = len2 - 1;
         if(len%2 == 1) {
-            return nums[half];
+            return findKth(nums1, nums2, 0, last1, 0, last2, half);
         } else {
-            return (nums[half]+nums[half-1])/2.0;
+            return (findKth(nums1, nums2, 0, last1, 0, last2, half)+findKth(nums1, nums2, 0, last1, 0, last2, half-1))*0.5;
         }
     }
     
@@ -31,33 +34,63 @@ public class Solution {
         return true;
     }
     
-    private int[] merge(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        int[] nums = new int[len1+len2];
-        int i1 = 0;
-        int i2 = 0;
-        int i = 0;
-        while(i1 < len1 && i2 < len2) {
-            if(nums1[i1] < nums2[i2]) {
-                nums[i] = nums1[i1];
-                i1++;
-            } else {
-                nums[i] = nums2[i2];
-                i2++;
-            }
-            i++;
+    private double findMedian(int[] nums) {
+        int len = nums.length;
+        int half = len/2;
+        if(len%2 == 1) {
+            return nums[half];
+        } else {
+            return (nums[half] + nums[half-1])*0.5;
         }
-        while(i1 < len1) {
-            nums[i] = nums1[i1];
-            i++;
-            i1++;
+    }
+    
+    private int findKth(int[] nums1, int[] nums2, int lo1, int hi1, 
+                            int lo2, int hi2, int k) {
+                     
+        if(lo1 > hi1) {
+            return nums2[lo2+k];
+        } else if(lo2 > hi2) {
+            return nums1[lo1+k];
+        } 
+           
+        if(k == 0) {
+            return Math.min(nums1[lo1], nums2[lo2]);
+        } 
+        
+        int len1 = hi1-lo1+1;
+        int len2 = hi2-lo2+1;
+        int len = len1 + len2;
+        
+        int k1 = k * len1/len;
+        int k2 = k - 1 - k1;
+        
+        int i1 = k1 + lo1;
+        int i2 = k2 + lo2;
+        if(nums1[i1] < nums2[i2]) {
+            k -= k1 + 1;
+            return findKth(nums1, nums2, lo1+k1+1, hi1, lo2, hi2, k);
+        } else {
+            k -= k2 + 1;
+            return findKth(nums1, nums2, lo1, hi1, lo2+k2+1, hi2, k);
         }
-        while(i2 < len2) {
-            nums[i] = nums2[i2];
-            i++;
-            i2++;
-        }
-        return nums;
     }
 }
+/*
+Submission Result: Runtime Error More Details 
+
+Runtime Error Message:
+Line 69: java.lang.ArrayIndexOutOfBoundsException: 1
+Last executed input:
+[2,3,4]
+[1]
+
+Submission Result: Wrong Answer More Details 
+
+Input:
+[1,2]
+[1,2]
+Output:
+2.00000
+Expected:
+1.50000
+*/
