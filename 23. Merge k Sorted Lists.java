84e3fe4aff937df1582cfd3449e7eb4ccfac3e25
shallwexuewei <wxuewei1991@gmail.com>
2016-01-28 22:59:37 -0500

    Update 23. Merge k Sorted Lists.java

--- 23. Merge k Sorted Lists.java
+++ 23. Merge k Sorted Lists.java
@@ -10,29 +10,34 @@ public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists == null || lists.length == 0)  return null;
         
-        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>(){
-            @Override
-            public int compare(ListNode a, ListNode b) {
-                return a.val - b.val;
-            }
-        });
-        
-        for(ListNode node:lists) {
-            if(node == null) {
-                continue;
-            }
-            queue.add(node);
+        return merge(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode merge(ListNode[] lists, int lo, int hi) {
+        if(lo == hi) {
+            return lists[lo];
         }
+        
+        int mid = lo + (hi - lo)/2;
+        ListNode list1 = merge(lists, lo, mid);
+        ListNode list2 = merge(lists, mid + 1, hi);
         
         ListNode fakeHead = new ListNode(0);
         ListNode prev = fakeHead;
-        while(!queue.isEmpty()) {
-            ListNode node = queue.poll();
-            prev.next = node;
-            if(node.next != null) {
-                queue.add(node.next);
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
             }
-            prev = node;
+            prev = prev.next;
+        }
+        if(list1 != null) {
+            prev.next = list1;
+        } else {
+            prev.next = list2;
         }
         return fakeHead.next;
     }
