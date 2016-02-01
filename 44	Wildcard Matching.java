eb96410a0c9ae89a2bc329bec5e77663fd639afa
shallwexuewei <wxuewei1991@gmail.com>
2016-01-31 21:35:03 -0500

    Update 44	Wildcard Matching.java

--- 44	Wildcard Matching.java
+++ 44	Wildcard Matching.java
@@ -1,64 +1,30 @@
 public class Solution {
-    public static boolean matchChar(char c, char p) {
-        return (p == '?' || p == c);
-    }
-    
-    public static boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-            return false;
-        }
-        
-        int si = 0;
-        int pi = 0;
-        
+    public boolean isMatch(String s, String p) {
         int lenS = s.length();
         int lenP = p.length();
         
-        int preS = 0;
-        int preP = 0;
+        boolean[][] dp = new boolean[lenS + 1][lenP + 1];
+        dp[0][0] = true;
         
-        boolean back = false;
+        for(int j = 1; j <= lenP; j++) {
+            if(p.charAt(j-1) == '*')    dp[0][j] = dp[0][j-1];
+            else                        break;
+        }
         
-        while (si < lenS) {
-            if (pi < lenP && matchChar(s.charAt(si), p.charAt(indexP))) {
-                si++;
-                pi++;
-            } else if (pi < lenP && p.charAt(pi) == '*') { // e.g. s:"aa", p:"*"
-            
-                // the function of continous '*' like "*****" equals to the single '*'
-                while (pi < lenP && p.charAt(pi) == '*') {
-                    pi++;
+        for(int i = 1; i <= lenS; i++) {
+            for(int j = 1; j <= lenP; j++) {
+                char charS = s.charAt(i-1);
+                char charP = p.charAt(j-1);
+                if(charP == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }
-                
-                //P的最后一个是 *，表示可以匹配任何字符串
-                if (indexP == lenP) {
-                    return true;
+                if(dp[i-1][j-1]) {
+                    if(charS == charP || charP == '?') {
+                        dp[i][j] = true;
+                    }
                 }
-                
-                // now we use '*' to match empty string
-                // but we record the positions, so if the assumption is wrong
-                // we can go back to the position to use '*' to match longer string
-                // 记录下这个匹配位置。
-                back = true;
-                preS = si;
-                preP = pi;
-            } else if (back) { // there is '*' before, we could try to use it to match longer string
-                si = ++preS;
-                pi = preP;
-            } else { // s is larger than p, e.g. s:"aa", p:"a"
-                return false;
             }
         }
-        
-        // 跳过末尾的所有的*.
-        while (pi < lenP && p.charAt(pi) == '*') {
-            pi++;
-        }
-        
-        if (si == lenS && indexP == lenP) {
-            return true;
-        }
-
-        return false;
+        return dp[lenS][lenP];
     }
 }
