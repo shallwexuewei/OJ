400ce362673b52955d04cb87489c79b27899f5e8
shallwexuewei <wxuewei1991@gmail.com>
2016-02-16 08:52:17 -0500

    Update and rename 33 Search in Rotated Sorted Array to 33 Search in Rotated Sorted Array.java

---
+++ 33 Search in Rotated Sorted Array.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)    return -1;
+        
+        int lo = 0;
+        int hi = nums.length - 1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] < nums[hi]) {
+                if(nums[mid] < target && nums[hi] > target) {
+                    lo = mid + 1;
+                } else if(nums[mid] == target) {
+                    return mid;
+                } else if(nums[hi] == target) {
+                    return hi;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                if(nums[lo] < target && nums[mid] > target) {
+                    hi = mid - 1;
+                } else if(nums[lo] == target) {
+                    return lo;
+                } else if(nums[mid] == target) {
+                    return mid;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+}
