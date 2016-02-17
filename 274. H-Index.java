9f091727d710618083c62aac6ae3e8e547e5c2bf
shallwexuewei <wxuewei1991@gmail.com>
2016-02-17 08:53:15 -0500

    Update 274. H-Index.java

--- 274. H-Index.java
+++ 274. H-Index.java
@@ -1,16 +1,28 @@
 public class Solution {
     public int hIndex(int[] citations) {
-        if(citations == null || citations.length == 0)  {
-            // ...
+        if(citations == null || citations.length == 0) {
+            return 0;
         }
         
-        Arrays.sort(citations);
-        
         int len = citations.length;
-        int cnt = len;
-        for(int i = 0; i < len; i++, cnt--) {
-            if(citations[i] >= cnt) {
-                return cnt;
+        int[] nums = new int[len + 1];
+        
+        
+        for(int c:citations) {
+            if(c > len) {
+                nums[len]++;
+            } else {
+                nums[c]++;
+            }
+        }
+        
+        if(nums[len] >= len) {
+            return len;
+        }
+        for(int i = len-1; i > -1; i--) {
+            nums[i] += nums[i+1];
+            if(nums[i] >= i) {
+                return i;
             }
         }
         return 0;
