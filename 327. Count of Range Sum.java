2766fd79d0464e7c7fee89be8ff4e286da3d78d7
shallwexuewei <wxuewei1991@gmail.com>
2016-02-19 09:24:48 -0500

    Update 327. Count of Range Sum.java

--- 327. Count of Range Sum.java
+++ 327. Count of Range Sum.java
@@ -1,12 +1,15 @@
 public class Solution {
     public int countRangeSum(int[] nums, int lower, int upper) {
         int n = nums.length;
+        // produce an array sums[i] storing the sum of nums[0:i-1]
+        // sums[0] is the sum of nothing, so the length of array sum is n + 1.
         long[] sums = new long[n + 1];
         sums[0] = 0;
         for (int i = 0; i < n; ++i)
             sums[i + 1] = sums[i] + nums[i];
-        System.out.println(Arrays.toString(sums));
-            
+        // System.out.println(Arrays.toString(sums));
+        
+        // merge sort needs auxiliary array to store the intermediate result    
         long[] aux = new long[n+1];
         return sort(sums, aux, 0, n, lower, upper);
     }
@@ -28,12 +31,15 @@ public class Solution {
         }
         
         // merge back to sums[]
+        // use two pointer to narrow down the window
         int count = 0;
         
-        // use two pointer to narrow down the window
+        // left, right, aux指针都在右边区间
         int left = mid + 1;
-        int right = left;
+        //int right = left;
         int auxI = left;
+        
+        // sumI， k指针在左边区间
         int sumsI = lo;
         
         // merge all values in the left part back to sums
@@ -43,14 +49,19 @@ public class Solution {
                 left++;
             }
             // find right boudary
+            int right = left;
             while(right <= hi && aux[right] - aux[k] <= upper) {
                 right++;
             } 
-            // real merge sort
+            
+            // real merge sort, 把所有比aux[k]小的右边区间的数都放进sums里
+            // 然后再把aux[k]放进sums里。
             while(auxI <= hi && aux[auxI] < aux[k]) {
                 sums[sumsI++] = aux[auxI++];
             }
             sums[sumsI++] = aux[k];
+            
+            // 为什么不用＋1？因为right的值是在窗口外的。
             count += right - left;
         }
         
