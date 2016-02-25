2fba9680d318cd8675bcdac5814e9442350dc1ec
shallwexuewei <wxuewei1991@gmail.com>
2016-02-24 23:46:21 -0500

    Update 293 Flip Game.java

--- 293 Flip Game.java
+++ 293 Flip Game.java
@@ -1,19 +1,22 @@
 public class Solution {
     public List<String> generatePossibleNextMoves(String s) {
-        int len = s.length();
-        List<String> result = new LinkedList<String>();
-        
-        if(len < 2) {
-            return result;
+        if(s == null)   {
+            //...
         }
         
-        boolean prevPlus = s.charAt(0) == '+' ? true:false;
-        for(int i = 1; i < len; i++) {
-            boolean current = s.charAt(i) == '+' ? true:false;
-            if(prevPlus && current) {
-                result.add(s.substring(0, i-1) + "--" + s.substring(i+1, len));
-            } 
-            prevPlus = current;
+        List<String> result = new ArrayList<String>();
+        char[] chars = s.toCharArray();
+        
+        for(int i = 1; i < s.length(); i++) {
+            if(chars[i] == '+') {
+                if(chars[i-1] == '+') {
+                    chars[i-1] = '-';
+                    chars[i] = '-';
+                    result.add(new String(chars));
+                    chars[i-1] = '+';
+                    chars[i] = '+';
+                }
+            }
         }
         return result;
     }
