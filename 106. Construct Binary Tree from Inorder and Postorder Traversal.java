f893bd545f45da50b95374c96c4a502c7de63389
shallwexuewei <wxuewei1991@gmail.com>
2016-03-29 06:24:42 -0700

    Update 106. Construct Binary Tree from Inorder and Postorder Traversal.java

--- 106. Construct Binary Tree from Inorder and Postorder Traversal.java
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -8,21 +8,21 @@
  * }
  */
 public class Solution {
-    HashMap<Integer, Integer> val2index;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int len = inorder.length;
         if(len == 0 || postorder.length != len) return null;
         
-        val2index = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> val2index = new HashMap<Integer, Integer>();
         for(int i = 0; i < len; i++) {
             val2index.put(inorder[i], i);
         }
         
-        return buildTree(inorder, 0, len - 1, postorder, 0, len - 1);
+        return buildTree(inorder, 0, len - 1, postorder, 0, len - 1, val2index);
     }
     
     private TreeNode buildTree(int[] inorder, int inLo, int inHi, 
-                                int[] postorder, int postLo, int postHi) {
+                                int[] postorder, int postLo, int postHi, 
+                                HashMap<Integer, Integer> val2index) {
         if(inLo == inHi)    return new TreeNode(inorder[inLo]);
         
         int rootVal = postorder[postHi];
@@ -34,11 +34,13 @@ public class Solution {
         
         if(leftSize != 0) {
             root.left = buildTree(inorder, inLo, rootInIndex - 1, 
-                                    postorder, postLo, postLo + leftSize - 1);
+                                    postorder, postLo, postLo + leftSize - 1, 
+                                    val2index);
         }
         if(rightSize != 0) {
             root.right = buildTree(inorder, rootInIndex + 1, inHi, 
-                                    postorder, postLo + leftSize, postHi - 1);
+                                    postorder, postLo + leftSize, postHi - 1, 
+                                    val2index);
         }
         return root;
     }
